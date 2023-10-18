5c71dc0a19b3dc026635e98b0b7e0cf24231675d
Azarel Grahandito <skootydack@gmail.com>
2023-10-18 08:30:09 +0700

    feat: add movie page

---
+++ script.js
@@ -0,0 +1,30 @@
+$.ajax({
+    type: "GET",
+    url: "https://it-its.id/api/movies",
+    success: function (data) {
+        console.log(data[0])
+    }
+}).then(function (data) {
+    x = 0;
+    var row = "#row" + x;
+    for (let i = 0; i < data.length; i++) {
+        if ($(row).children().length >= 2) {
+            x += 1;
+            row = "#row" + x
+            $(".container").append(`<div class="row justify-content-center align-items-center g-0" id="row${x}"> 
+         </div>`)
+        }
+        $(row).append(`<div class="col-lg p-2">
+        <div class="card">
+            <div style="box-shadow: 0px 5px 10px 1px #D3D3D3; padding: 35px" class="card-body">
+                <h5 class="card-title">${data[i].Title}</h5>
+                <p class="card-text">${data[i].Plot}</p>
+            </div>
+        </div>
+    </div>`)
+    }
+})
+
+function render(data) {
+
+}
